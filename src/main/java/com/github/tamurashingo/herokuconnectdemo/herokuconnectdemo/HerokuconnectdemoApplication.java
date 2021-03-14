af3a990e06307ff1565bd695761b232c43c905e1
tamura shingo <tamura.shingo@gmail.com>
2021-03-14 21:54:26 +0900

    generated from spring initializr

---
+++ src/main/java/com/github/tamurashingo/herokuconnectdemo/herokuconnectdemo/HerokuconnectdemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.tamurashingo.herokuconnectdemo.herokuconnectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuconnectdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuconnectdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/tamurashingo/herokuconnectdemo/herokuconnectdemo/HerokuconnectdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tamurashingo.herokuconnectdemo.herokuconnectdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuconnectdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
