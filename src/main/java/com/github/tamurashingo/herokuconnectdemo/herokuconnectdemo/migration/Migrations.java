f1a620442286b5c9280ae7b1cfad714d192b538c
tamura shingo <tamura.shingo@gmail.com>
2021-03-15 15:32:40 +0900

    typo

--- src/main/java/com/github/tamurashingo/herokuconnectdemo/herokuconnectdemo/migration/Migrations.java
+++ src/main/java/com/github/tamurashingo/herokuconnectdemo/herokuconnectdemo/migration/Migrations.java
@@ -7,7 +7,7 @@ import org.flywaydb.core.Flyway;
  */
 public class Migrations {
     public static void main(String...args) throws Exception {
-        Flyway flyway = Flyway.configure().dataSource(System.getenv("${JDBC_DATABASE_URL"),
+        Flyway flyway = Flyway.configure().dataSource(System.getenv("JDBC_DATABASE_URL"),
                 System.getenv("JDBC_DATABASE_USERNAME"),
                 System.getenv("JDBC_DATABASE_PASSWORD")).load();
 
